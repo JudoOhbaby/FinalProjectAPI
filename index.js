eb1ef2948be2b80d0294661cfd87dc30fa71bcb5
JudoOhbaby <rattapon_um@kkumail.com>
2022-10-07 22:33:03 +0700

    Seven Version

--- index.js
+++ index.js
@@ -1,28 +1,30 @@
 const express = require('express')
-
+const database = require('./Database/Firebase')
+const db = database.database
 const app = express()
 const PORT = 4000
 
-app.listen(PORT,()=>{
+app.listen(PORT, () => {
     console.log('API Listening on PORT ${PORT}')
 })
 
-app.get('/',(req,res)=>{
+app.get('/', (req, res) => {
     res.send('API READY')
 })
 
-app.get('/Test',(req,res)=>{
+app.get('/Test', (req, res) => {
     res.send('testing..')
 })
 
-app.get('/TestFirebsae',(req,res)=>{
-    try{
-        set(ref(db,'test/'),{
-            name:12
-        })
-    }catch(e){
-        console.log(e)
-    }
+app.get('/TestFirebase', (req, res) => {
+    db.ref().child('/test').update({ name: 2 }, (error) => {
+        if (error) {
+            console.log('error')
+        } else {
+            console.log('sucess')
+            res.send('success')
+        }
+    })
 })
 
 module.exports = app
